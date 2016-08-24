4b69f7403a32191b65adf9c5c286f04b2b11b930
Josefino <josefinocutecua@gmail.com>
2016-08-24 09:00:42 +0200

    reordenamos o codigo

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -9,48 +9,48 @@ public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
-		
+
 		TipoDeEspetaculo tipoDeEspectaculo = sessao.getEspetaculo().getTipo();
-		
-		double razaoIngressosRestantes = (sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue();
-		
+
+		double razaoIngressosRestantes = (sessao.getTotalIngressos() - sessao.getIngressosReservados())
+				/ sessao.getTotalIngressos().doubleValue();
+
 		switch (tipoDeEspectaculo) {
 		case CINEMA:
 		case SHOW:
-			if(razaoIngressosRestantes <= 0.05) { 
+			if (razaoIngressosRestantes <= 0.05) {
 				preco = sessao.getPreco().add(precoPeloValorDaRazao(sessao, 0.10));
-			} else {
+			} 
+			else {
 				preco = sessao.getPreco();
 			}
 			break;
-			
+
 		case BALLET:
 		case ORQUESTRA:
-			if(razaoIngressosRestantes <= 0.50) { 
+			if (razaoIngressosRestantes <= 0.50) {
 				preco = sessao.getPreco().add(precoPeloValorDaRazao(sessao, 0.20));
-			} else {
+			} 
+			else {
 				preco = sessao.getPreco();
 			}
-			
-			if(sessao.getDuracaoEmMinutos() > 60){
+
+			if (sessao.getDuracaoEmMinutos() > 60) {
 				preco = preco.add(precoPeloValorDaRazao(sessao, 0.10));
 			}
-			
+
 			break;
 
 		default:
 			preco = sessao.getPreco();
 			break;
 		}
-			
-		
+
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
 
 	private static BigDecimal precoPeloValorDaRazao(Sessao sessao, double valorDaRazao) {
 		return sessao.getPreco().multiply(BigDecimal.valueOf(valorDaRazao));
 	}
-	
-	
 
 }
